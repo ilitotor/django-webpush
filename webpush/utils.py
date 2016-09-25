9f856edf595698d16f5368e0a0fe1b98eb3cd0a6
jamaalscarlett <jamaal.scarlett@gmail.com>
2016-09-25 13:42:11 -0400

    Fixed Django 1.9+ compatiablity errors. Added .gitignore file

--- webpush/utils.py
+++ webpush/utils.py
@@ -1,6 +1,3 @@
-# import requests
-from .models import PushInformation, Group
-
 from django.forms.models import model_to_dict
 
 from pywebpush import WebPusher
@@ -14,7 +11,7 @@ def send_notification_to_user(user, payload, ttl=0):
 
 
 def send_notification_to_group(group_name, payload, ttl=0):
-
+    from .models import Group
     # Get all the subscription related to the group
     push_infos = Group.objects.get(name=group_name).webpush_info.select_related("subscription")
     for push_info in push_infos:
